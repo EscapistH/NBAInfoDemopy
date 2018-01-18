2e01c0d80e9d2af9a94285fe56e952d731625e3f
EscapistH <923737723@qq.com>
2018-01-19 04:55:11 +0800

    Alpha 2.0

--- data.py
+++ data.py
@@ -1,11 +1,144 @@
 # coding:utf-8
 
-def ShowData():
-    print('------------------------')
-    print('姓名:',list[i]['姓名'])
-    print('球队:',list[i]['球队'])
-    print('球衣号码:',list[i]['球衣号码'])
-    print('擅长位置:',list[i]['位置'])
-    print('身高:',list[i]['身高'])
-    print('------------------------\n')
+with open('DataTest.data','r',encoding='utf-8') as f:
+    list = eval(f.read())
 
+def createData(name):
+        message = {}
+        message['姓名'] = name
+        message['球队'] = input('请输入球员所属球队:')
+        message['球衣号码'] = input('请输入球员的球衣号码:')
+        message['位置'] = input('请输入球员擅长位置:')
+        message['身高'] = input('请输入球员身高:')
+        list.append(message)
+        with open('DataTest.data','w',encoding='utf-8') as f:
+            f.write(str(list))
+        print('添加成功!\n')
+
+def delteData(name):
+    i = 0
+    length = len(list)
+    while i < length:
+        if list[i]['姓名'] == name:
+            flag = input('已找到'+name+'的数据,确认删除请输入Y,否则请按任意键.')
+            if flag == 'Y':
+                del list[i]
+                print('删除成功!\n')
+                with open('DataTest.data','w',encoding='utf-8') as f:
+                    f.write(str(list))
+                break
+            else:
+                print('用户取消!\n')
+                break
+        else:
+            if i == (length-1):
+                print('未查询到'+name+'的数据,操作失败.\n')
+                break
+        i += 1
+
+def changeTeam(name):
+    i = 0
+    length = len(list)
+    while i < length:
+        if list[i]['姓名'] == name:
+            list[i]['球队'] = input('请输入球员现役球队:')
+            with open('DataTest.data','w',encoding='utf-8') as f:
+                f.write(str(list))
+            print('修改成功!\n')
+            break
+        i += 1
+
+def changeNumber(name):
+    i = 0
+    length = len(list)
+    while i < length:
+        if list[i]['姓名'] == name:
+            list[i]['球衣号码'] = input('请输入球员现在的球衣号码:')
+            with open('DataTest.data','w',encoding='utf-8') as f:
+                f.write(str(list))
+            print('修改成功!\n')
+            break
+        i += 1
+
+def changeSeat(name):
+    i = 0
+    length = len(list)
+    while i < length:
+        if list[i]['姓名'] == name:
+            list[i]['位置'] = input('请输入球员现在担任的位置:')
+            with open('DataTest.data','w',encoding='utf-8') as f:
+                f.write(str(list))
+            print('修改成功!\n')
+            break
+        i += 1
+
+def changeHeight(name):
+    i = 0
+    length = len(list)
+    while i < length:
+        if list[i]['姓名'] == name:
+            list[i]['身高'] = input('请输入球员现在的身高:')
+            with open('DataTest.data','w',encoding='utf-8') as f:
+                f.write(str(list))
+            print('修改成功!\n')
+            break
+        i += 1
+
+def changeAll(name):
+    i = 0
+    length = len(list)
+    while i < length:
+        if list[i]['姓名'] == name:
+            list[i]['球队'] = input('请输入球员现役球队:')
+            list[i]['球衣号码'] = input('请输入球员现在的球衣号码:')
+            list[i]['位置'] = input('请输入球员现在担任的位置:')
+            list[i]['身高'] = input('请输入球员现在的身高:')
+            with open('DataTest.data','w',encoding='utf-8') as f:
+                f.write(str(list))
+            print('修改成功!\n')
+            break
+        i += 1
+
+def searchData(name):
+    i = 0
+    length = len(list)
+    while i < length:
+        if list[i]['姓名'] == name:
+            print('\n+------------+--------------------+')
+            print('|  姓    名  | ',list[i]['姓名'])
+            print('+------------|--------------------+')
+            print('|  球    队  | ',list[i]['球队'])
+            print('+------------|--------------------+')
+            print('|  球衣号码  | ',list[i]['球衣号码'])
+            print('+------------|--------------------+')
+            print('|  擅长位置  | ',list[i]['位置'])
+            print('+------------|--------------------+')
+            print('|  身    高  | ',list[i]['身高'])
+            print('+------------+--------------------+\n')
+            with open('DataTest.data','r',encoding='utf-8') as f:
+                f.read()
+            break
+        else:
+            if i == (length-1):
+                print('未查询到'+name+'的数据.\n')
+                break
+        i += 1
+
+def searchAll():
+    i = 0
+    length = len(list)
+    while i < length:
+        print('\n+------------+--------------------+')
+        print('|  姓    名  | ',list[i]['姓名'])
+        print('+------------|--------------------+')
+        print('|  球    队  | ',list[i]['球队'])
+        print('+------------|--------------------+')
+        print('|  球衣号码  | ',list[i]['球衣号码'])
+        print('+------------|--------------------+')
+        print('|  擅长位置  | ',list[i]['位置'])
+        print('+------------|--------------------+')
+        print('|  身    高  | ',list[i]['身高'])
+        print('+------------+--------------------+\n')
+        with open('DataTest.data','r',encoding='utf-8') as f:
+            f.read()
+        i += 1
--- main.py
+++ main.py
@@ -3,6 +3,101 @@
 import menu
 import data
 
-menu.ShowMenu()
-with open('Database.data','r') as f:
-    print(f.read())
+menu.mainMenu()
+
+with open('DataTest.data','r',encoding='utf-8') as f:
+    list = eval(f.read())
+
+def main():
+    while(True):
+
+        command = input('请输入要进行的操作(输入9可以呼出菜单):')
+        if command == '1':
+            # 增加
+            print('\n')
+            name = input('请输入要添加的球员姓名:')
+            i = 0
+            length = len(list)
+            while i < length:
+                if list[i]['姓名'] == name:
+                    print('已查询到'+name+'的数据,添加失败!')
+                    flag = input('要显示'+name+'的数据请输入Y,否则请按任意键退出.')
+                    if flag == 'Y':
+                        data.searchData(name)
+                        break
+                    else:
+                        print('用户取消!\n')
+                        break
+                else:
+                    if i == (length-1):
+                        data.createData(name)
+                i += 1
+
+        elif command == '2':
+            # 删除
+            print('\n')
+            name = input('请输入要删除数据的球员姓名:')
+            data.delteData(name)
+
+        elif command == '3':
+            # 改变
+            print('\n')
+            name = input('请输入要修改数据的球员姓名:')
+            i = 0
+            length = len(list)
+            while i < length:
+                if list[i]['姓名'] != name:
+                    if i == (length-1):
+                        print('未查询到'+name+'的数据!\n')
+                else:
+                    print('-------------')
+                    print(' 1. 球队\n 2. 球衣号码\n 3. 位置\n 4. 身高\n 5. 以上所有\n 0. 取消')
+                    print('-------------')
+                    key = input('请输入要修改的数据:')
+                    if key == '1':
+                        data.changeTeam(name)
+                        break
+                    
+                    elif key == '2':
+                        data.changeNumber(name)
+                        break
+
+                    elif key == '3':
+                        data.changeSeat(name)
+                        break
+
+                    elif key == '4':
+                        data.changeHeight(name)
+                        break
+
+                    elif key == '5':
+                        data.changeAll(name)
+                        break
+                    
+                    elif key == '0':
+                        break
+                i += 1
+
+        elif command == '4':
+            # 查询某一条数据
+            print('\n')
+            name = input('请输入要查询的球员姓名:')
+            data.searchData(name)
+
+        elif command == '5':
+            # 查询所有数据
+            data.searchAll()
+
+        elif command == '9':
+            # 显示菜单
+            menu.mainMenu()
+
+        elif command == '0':
+            # 退出
+            break
+
+        else:
+            print('输入错误,请重新输入!')
+
+if __name__ == '__main__':
+    main()
